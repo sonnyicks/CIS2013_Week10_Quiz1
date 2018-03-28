bf0554d2660b9264711f103115f9eda8a6756c6f
sonnyicks <35547157+sonnyicks@users.noreply.github.com>
2018-03-28 18:43:00 -0500

    final

--- CIS2013_Week10_Quiz1.cpp
+++ CIS2013_Week10_Quiz1.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
-#include <string>
+#include <cstring>
 
 using namespace std;
 
+
+
+int count = 1;
+
 int main(){
+	string user_line;
+
 	
+	cout << "Type something: ";
+	getline(cin, user_line);
+	int a=user_line.length();
+
+	for (int i=0; i<a; i++){
+		if ((user_line[i]=='b')&&(count%2==1)){
+			user_line[i]='S';
+			cout << user_line[i] << "IT";
+			count++;
+		}
+		else if ((user_line[i]=='b')&&(count%2==0)){
+			user_line[i]='S';
+			cout << user_line[i] << "TAND";
+			count++;
+		}
+		else{cout << user_line[i];}
+
+	}
+
 	return 0;
 }
